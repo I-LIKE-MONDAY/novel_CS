bbf5bb87f2596bb649ab125906c7c621338f3c78
DSTECH-HP03-ELI\DSTECH <nmk02@naver.com>
2023-04-05 18:22:20 +0900

    2023-04-05

--- DS_GWAK_CS_Study/Program.cs
+++ DS_GWAK_CS_Study/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Channels;
+using System.Xml;
+
+public class Family
+{
+	public int age;
+	public long height;
+	private float weight; // private
+}// class
+
+public class AccessTest
+{
+	public static void Main()
+	{
+		Family sister = new Family(); // 메모리가 생성된 상태
+		sister.age = 100;
+		sister.height = 170L;
+		//sister.weight = 67.0F; // 에러 발생 : private는 클래스 내부에서만 사용
+	}
+}
+
+
---
+++ DS_GWAK_CS_Study/TestPrivateMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+public class TestPrivate
+{
+	private int top_secret;
+
+	public void SetWeight(int my_weight)
+	{
+		top_secret = my_weight;
+	}
+
+	public int GetWeight()
+	{
+		return top_secret;
+	}
+}// class
+
+public class TestPrivateMain
+{
+	public static void Main()
+	{
+		int s;
+		TestPrivate we = new TestPrivate();
+		we.SetWeight(70);// 메서드의 매개변수를 이용한 private 값 셋팅
+		s = we.GetWeight();// 메서드의 리턴을 이용하여 private 데이터 얻기 : 간접적으로 접근
+		Console.WriteLine("private 멤버의 값은 : " + we.GetWeight());
+		Console.WriteLine("private 멤버의 값은 : " + s);
+	}
+}
